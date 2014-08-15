b40f5b5eeb1448c3299c307b74a2750d8524f04a
Mevan Samaratunga <mevan.samaratunga@fmr.com>
2014-08-14 22:54:49 -0400

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'openstack-services'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures openstack-services'
+long_description 'Installs/Configures openstack-services'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: openstack-services
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
